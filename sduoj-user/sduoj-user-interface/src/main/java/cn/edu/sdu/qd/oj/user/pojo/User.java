dfbe42f48f1eea883d6cafaaf8741278c9af3447
zhangt2333 <zhangt2333@gmail.com>
2020-03-12 21:32:06 +0800

    Add the data validator of `User`.

--- sduoj-user/sduoj-user-interface/src/main/java/cn/edu/sdu/qd/oj/user/pojo/User.java
+++ sduoj-user/sduoj-user-interface/src/main/java/cn/edu/sdu/qd/oj/user/pojo/User.java
@@ -6,12 +6,17 @@
 package cn.edu.sdu.qd.oj.user.pojo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @ClassName User
@@ -27,22 +32,34 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "u_id")
     private Integer id;
 
     @Column(name = "u_username")
+    @Length(min = 4, max = 16, message = "用户名长度必须在4-16位之间")
+    @NotBlank(message = "用户名不能为空")
     private String username;
 
     @Column(name = "u_password")
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Length(min = 4, max = 32, message = "密码长度必须在4-32位之间")
+    @NotBlank(message = "密码不能为空")
     private String password;
 
     @Column(name = "u_nickname")
+    @NotBlank(message = "昵称不能为空")
     private String nickname;
 
     @Column(name = "u_email")
+    @Email(message = "邮箱不合法")
+    @NotBlank(message = "邮箱不能为空")
     private String email;
 
     @Column(name = "u_gender")
+    @Range(min = 0, max = 2, message = "性别不合法, 0.女, 1.男, 2.问号")
     private Short gender;
+
+    @Column(name = "u_student_id")
+    private String studentId;
 }
