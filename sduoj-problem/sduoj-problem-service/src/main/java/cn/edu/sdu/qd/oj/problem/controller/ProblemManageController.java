c484f57ac9c1e8d99a83c44c3c98b9810da49ef1
Xrvitd <30816779+Xrvitd@users.noreply.github.com>
2020-04-10 20:28:18 +0800

    Complete the API of problem creating.

--- sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/controller/ProblemManageController.java
+++ sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/controller/ProblemManageController.java
@@ -5,15 +5,24 @@
 
 package cn.edu.sdu.qd.oj.problem.controller;
 
+import cn.edu.sdu.qd.oj.auth.entity.UserInfo;
+import cn.edu.sdu.qd.oj.auth.utils.JwtUtils;
 import cn.edu.sdu.qd.oj.common.entity.ApiResponseBody;
+import cn.edu.sdu.qd.oj.common.enums.ApiExceptionEnum;
+import cn.edu.sdu.qd.oj.common.exception.ApiException;
+import cn.edu.sdu.qd.oj.common.utils.CookieUtils;
+import cn.edu.sdu.qd.oj.problem.config.JwtProperties;
 import cn.edu.sdu.qd.oj.problem.pojo.ProblemManageBo;
 import cn.edu.sdu.qd.oj.problem.service.ProblemManageService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -25,11 +34,15 @@ import java.util.Map;
  **/
 
 @Controller
+@EnableConfigurationProperties(JwtProperties.class)
 @RequestMapping("/manage/problem")
 public class ProblemManageController {
 
     @Autowired
     private ProblemManageService problemManageService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
 
 
     @PostMapping("/query")
@@ -38,4 +51,20 @@ public class ProblemManageController {
         return this.problemManageService.queryById((Integer) json.get("problemId"));
     }
 
+    @PostMapping("/create")
+    @ApiResponseBody
+    public Integer createProblemManageBo(@RequestBody ProblemManageBo problem, HttpServletRequest request)
+    {
+        // TODO: 鉴权
+        String token = CookieUtils.getCookieValue(request, this.jwtProperties.getCookieName());
+        UserInfo userInfo;
+        try {
+            userInfo = JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+        } catch (Exception e) {
+            throw new ApiException(ApiExceptionEnum.UNKNOWN_ERROR);
+        }
+        problem.setUserId(userInfo.getUserId());
+        this.problemManageService.createProblem(problem);
+        return problem.getProblemId();
+    }
 }
--- sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/service/ProblemManageService.java
+++ sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/service/ProblemManageService.java
@@ -5,6 +5,8 @@
 
 package cn.edu.sdu.qd.oj.problem.service;
 
+import cn.edu.sdu.qd.oj.common.enums.ApiExceptionEnum;
+import cn.edu.sdu.qd.oj.common.exception.ApiException;
 import cn.edu.sdu.qd.oj.problem.mapper.ProblemManageBoMapper;
 import cn.edu.sdu.qd.oj.problem.pojo.ProblemManageBo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +27,13 @@ public class ProblemManageService {
 
     public ProblemManageBo queryById(Integer problemId) {
         return this.problemManageBoMapper.selectByPrimaryKey(problemId);
+    }
+
+    public boolean createProblem(ProblemManageBo problem) {
+        problem.setProblemId(null);
+        if (this.problemManageBoMapper.insertSelective(problem) != 1) {
+            throw new ApiException(ApiExceptionEnum.UNKNOWN_ERROR);
+        }
+        return true;
     }
 }
