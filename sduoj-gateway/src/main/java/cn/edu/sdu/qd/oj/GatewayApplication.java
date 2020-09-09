b1a434519673182e9b8fb12028adf707417cb601
zhangt2333 <zhangt2333@gmail.com>
2020-09-09 19:00:05 +0800

    [ISSUE #4] import Nacos. (#6)
    * [ISSUE #4] auth, user, problem, submit, gateway module switch to Nacos.
    
    * [ISSUE #4] remove config, registry module.

--- sduoj-gateway/src/main/java/cn/edu/sdu/qd/oj/GatewayApplication.java
+++ sduoj-gateway/src/main/java/cn/edu/sdu/qd/oj/GatewayApplication.java
@@ -7,7 +7,7 @@ package cn.edu.sdu.qd.oj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @ClassName GatewayApplication
@@ -18,7 +18,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
