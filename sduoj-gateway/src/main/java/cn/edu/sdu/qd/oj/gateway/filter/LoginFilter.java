fd61487b084dd711fb1bcc8ff5547200b87407f4
zhangt2333 <zhangt2333@gmail.com>
2020-10-04 11:49:23 +0800

    polish the code in gateway. (#40)
    * Allow the public api url to carry session.
    * Polish the code in gateway

--- sduoj-gateway/src/main/java/cn/edu/sdu/qd/oj/gateway/filter/LoginFilter.java
+++ sduoj-gateway/src/main/java/cn/edu/sdu/qd/oj/gateway/filter/LoginFilter.java
@@ -67,62 +67,72 @@ public class LoginFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         String requestUrl = exchange.getRequest().getPath().toString();
         log.info("filter url:{}", requestUrl);
-        if (!isAllowPath(requestUrl)) {
-            // 取 token 并解密
-            Object userinfo = Optional.ofNullable(exchange.getSession().block()).map(WebSession::getAttributes).map(map -> map.get("SDUOJUserInfo")).orElse(null);
-            if (userinfo != null) {
-                UserSessionDTO userSessionDTO = JSON.parseObject((String) userinfo, UserSessionDTO.class);
-                if (userSessionDTO != null) {
-                    // 对 url 鉴权
-                    List<String> roles = userClient.queryRolesById(Long.valueOf(userSessionDTO.getUserId()));
-                    // TODO: 缓存权限表
-                    Map<String, List<String>> urlToRolesMap = permissionClient.listAll().stream().collect(Collectors.toMap(PermissionDTO::getUrl, PermissionDTO::getRoles, (k1, k2) -> k1));
-                    List<String> urlRoles = urlToRolesMap.get(requestUrl.replace("/api", ""));
-                    // TODO: 魔法值解决
-                    if (!urlRoles.contains("all")
-                        && (roles != null && urlRoles != null && Collections.disjoint(roles, urlRoles))) {
-                        log.warn("have not permission {} {}", userSessionDTO, requestUrl);
-                        return returnNoPermission(exchange, String.format("This User has no permission on '%s'", requestUrl));
-                    }
-                    // 装饰器 修改 getHeaders 方法
-                    ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
-                        @Override
-                        public HttpHeaders getHeaders() {
-                            MultiValueMap<String, String> multiValueMap = CollectionUtils.toMultiValueMap(new LinkedCaseInsensitiveMap(8, Locale.ENGLISH));
-                            super.getHeaders().forEach((key, value) -> multiValueMap.put(key, value));
-//                            multiValueMap.remove("cookie"); // 在此处已解码 token, 故不下传省流量, 如果后续有多值 cookie 此处需要修改
-                            multiValueMap.remove("SDUOJUserInfo");
-                            multiValueMap.add("SDUOJUserInfo", JSON.toJSONString(userSessionDTO));
-                            for (Field field : UserSessionDTO.class.getDeclaredFields()) {
-                                try {
-                                    field.setAccessible(true);
-                                    multiValueMap.remove("Authorization-" + field.getName());
-                                    multiValueMap.add("Authorization-" + field.getName(), String.valueOf(field.get(userSessionDTO)));
-                                } catch (IllegalAccessException e) {
-                                    log.error("getHeaders Decorator", e);
-                                }
-                            }
-                            return new HttpHeaders(multiValueMap);
-                        }
-                    };
-                    return chain.filter(exchange.mutate().request(decorator).build());
-                }
-            }
-        } else {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                List<String> userInfos = exchange.getResponse().getHeaders().remove("SDUOJUserInfo");
-                Optional.of(userInfos).filter(list -> !list.isEmpty()).map(list -> list.get(0)).ifPresent(userInfoStr -> {
-                    Optional.ofNullable(exchange.getSession().block()).map(WebSession::getAttributes).ifPresent(map -> {
-                        if ("Logout".equals(userInfoStr)) {
-                            map.remove("SDUOJUserInfo");
-                        } else {
-                            map.put("SDUOJUserInfo", userInfoStr);
-                        }
-                    });
-                });
-            }));
+        // 取 token 并解密
+        UserSessionDTO userSessionDTO = Optional.of(exchange)
+                                    .map(ServerWebExchange::getSession)
+                                    .map(Mono::block)
+                                    .map(WebSession::getAttributes)
+                                    .map(map -> map.get("SDUOJUserInfo"))
+                                    .map(o -> (String) o)
+                                    .map(o -> JSON.parseObject(o, UserSessionDTO.class))
+                                    .orElse(null);
+        // 无 session，非 allowUrl
+        if (userSessionDTO == null && !isAllowPath(requestUrl)) {
+            return returnNoPermission(exchange, "鉴权失败，无 session");
         }
-        return returnNoPermission(exchange, "鉴权失败，无 session");
+
+
+        // 鉴权
+        if (userSessionDTO != null) {
+            List<String> roles = userClient.queryRolesById(userSessionDTO.getUserId());
+            // TODO: 缓存权限表
+            Map<String, List<String>> urlToRolesMap = permissionClient.listAll().stream().collect(Collectors.toMap(PermissionDTO::getUrl, PermissionDTO::getRoles, (k1, k2) -> k1));
+            List<String> urlRoles = urlToRolesMap.get(requestUrl.replace("/api", ""));
+            // TODO: 魔法值解决
+            if (!urlRoles.contains("all") &&
+                (roles != null && urlRoles != null && Collections.disjoint(roles, urlRoles))) {
+                log.warn("have not permission {} {}", userSessionDTO, requestUrl);
+                return returnNoPermission(exchange, String.format("This User has no permission on '%s'", requestUrl));
+            }
+            // 装饰器 修改 getHeaders 方法
+            ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                @Override
+                public HttpHeaders getHeaders() {
+                    MultiValueMap<String, String> multiValueMap = CollectionUtils.toMultiValueMap(new LinkedCaseInsensitiveMap(8, Locale.ENGLISH));
+                    super.getHeaders().forEach((key, value) -> multiValueMap.put(key, value));
+    //              multiValueMap.remove("cookie"); // 在此处已解码 token, 故不下传省流量, 如果后续有多值 cookie 此处需要修改
+                    multiValueMap.remove("SDUOJUserInfo");
+                    multiValueMap.add("SDUOJUserInfo", JSON.toJSONString(userSessionDTO));
+                    for (Field field : UserSessionDTO.class.getDeclaredFields()) {
+                        try {
+                            field.setAccessible(true);
+                            multiValueMap.remove("Authorization-" + field.getName());
+                            multiValueMap.add("Authorization-" + field.getName(), String.valueOf(field.get(userSessionDTO)));
+                        } catch (IllegalAccessException e) {
+                            log.error("getHeaders Decorator", e);
+                        }
+                    }
+                    return new HttpHeaders(multiValueMap);
+                }
+            };
+            return chain.filter(exchange.mutate().request(decorator).build()).then(thenHandlerSession(exchange));
+        }
+        return chain.filter(exchange).then(thenHandlerSession(exchange));
+    }
+
+    private Mono<Void> thenHandlerSession(ServerWebExchange exchange) {
+        return Mono.fromRunnable(() -> {
+            List<String> userInfos = exchange.getResponse().getHeaders().remove("SDUOJUserInfo");
+            Optional.of(userInfos).filter(list -> !list.isEmpty()).map(list -> list.get(0)).ifPresent(userInfoStr -> {
+                Optional.of(exchange).map(ServerWebExchange::getSession).map(Mono::block).map(WebSession::getAttributes).ifPresent(map -> {
+                    if ("Logout".equals(userInfoStr)) {
+                        map.remove("SDUOJUserInfo");
+                    } else {
+                        map.put("SDUOJUserInfo", userInfoStr);
+                    }
+                });
+            });
+        });
     }
 
     private Mono<Void> returnNoPermission(ServerWebExchange exchange, String msg) {
@@ -131,7 +141,7 @@ public class LoginFilter implements GlobalFilter, Ordered {
         Map<String, Object> message = new HashMap<>();
         message.put("code", HttpStatus.UNAUTHORIZED.value());
         message.put("message", msg);
-        message.put("timestamp", (int) (System.currentTimeMillis() / 1000));
+        message.put("timestamp", String.valueOf(System.currentTimeMillis()));
         message.put("data", null);
         byte[] bits = JSON.toJSONBytes(message);
         DataBuffer buffer = response.bufferFactory().wrap(bits);
