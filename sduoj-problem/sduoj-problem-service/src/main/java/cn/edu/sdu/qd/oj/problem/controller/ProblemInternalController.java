ef998900dc74e3b46d63427337b80839bf141be0
zhangt2333 <zhangt2333@gmail.com>
2021-01-01 09:58:08 +0800

    happy new year (#116)

--- sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/controller/ProblemInternalController.java
+++ sduoj-problem/sduoj-problem-service/src/main/java/cn/edu/sdu/qd/oj/problem/controller/ProblemInternalController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2020 the original author or authors.
+ * Copyright 2020-2021 the original author or authors.
  *
  * Licensed under the General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
