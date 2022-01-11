3001c387d8bee89187414194af39c2832c10ddd2
Teng Zhang <zhangt2333@gmail.com>
2022-01-11 13:08:00 +0800

    docs: replace GPLv3 with AGPLv3

--- sduoj-common/sduoj-common-web/src/main/java/cn/edu/sdu/qd/oj/NoWarnMapper.java
+++ sduoj-common/sduoj-common-web/src/main/java/cn/edu/sdu/qd/oj/NoWarnMapper.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2020-2021 the original author or authors.
+ * Copyright 2020-2022 the original author or authors.
  *
- * Licensed under the General Public License, Version 3.0 (the "License");
+ * Licensed under the Affero General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      https://www.gnu.org/licenses/gpl-3.0.en.html
+ *      https://www.gnu.org/licenses/agpl-3.0.en.html
  */
 
 package cn.edu.sdu.qd.oj;
